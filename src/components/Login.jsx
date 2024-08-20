5cc266304260c22a4b6cf2ba1c6b052651afddc4
Aditya sharma <sharmaaaditya167@gmail.com>
2024-08-20 08:08:32 +0530

    fixed issues

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     }
 
     try {
-      const res = await axios.post('http://localhost:5000/api/login', { contact, password });
+      const res = await axios.post('https://ssmss-backend.onrender.com/api/login', { contact, password });
       console.log(res.data)
       const user = res.data.data[0];
      
